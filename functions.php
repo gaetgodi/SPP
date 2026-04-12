6f852ac5d6a40c31f8a1d61a71337f352a2b09e3
Gaetan Godin <gaetgodi@gmail.com>
2026-04-12 13:29:57 -0400

    Add FAQ and testimonials system - mu-plugins, CSS, JS

--- functions.php
+++ functions.php
@@ -4,6 +4,23 @@ function divi_spp_child_enqueue_styles() {
     wp_enqueue_style( 'divi-parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'spp-drawers', get_stylesheet_directory_uri() . '/css/spp-drawers.css', [], '1.0.0' );
     wp_enqueue_script( 'spp-drawers', get_stylesheet_directory_uri() . '/js/spp-drawers.js', [], '1.0.0', true );
+
+    // FAQ System (CSS + JS - only on FAQ page)
+    if (is_page('faq')) {
+        wp_enqueue_style(
+            'spp-faq',
+            get_stylesheet_directory_uri() . '/css/faq.css',
+            [],
+            filemtime(get_stylesheet_directory() . '/css/faq.css')
+        );
+        wp_enqueue_script(
+            'spp-faq',
+            get_stylesheet_directory_uri() . '/js/faq-system.js',
+            [],
+            filemtime(get_stylesheet_directory() . '/js/faq-system.js'),
+            true
+        );
+    }
 }
 add_action( 'wp_enqueue_scripts', 'divi_spp_child_enqueue_styles' );
 
