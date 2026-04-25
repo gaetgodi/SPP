a7a467dccc602f328aa8e5a438f3e73edd9735ba
Gaetan Godin <gaetgodi@gmail.com>
2026-04-25 13:01:56 -0400

    Create default template for event registrations

--- single-tribe_events.php
+++ single-tribe_events.php
@@ -2,13 +2,17 @@
 get_header();
 ?>
 <div style="display:flex; gap:20px;">
-    <div style="flex:3;">
+    <div style="flex:3; background:yellow;">
+        COLUMN 1 START
         <?php while ( have_posts() ) : the_post(); ?>
             <?php tribe_get_template_part( 'single-event' ); ?>
         <?php endwhile; ?>
+        COLUMN 1 END
     </div>
-    <div style="flex:1;">
+    <div style="flex:1; background:lightblue;">
+        COLUMN 2 START
         <?php echo do_shortcode('[spp_side_nav]'); ?>
+        COLUMN 2 END
     </div>
 </div>
 <?php
