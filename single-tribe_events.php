ec90d6dcbc6b72b2d4f42ad4a107abddbf35bdcd
Gaetan Godin <gaetgodi@gmail.com>
2026-04-25 12:03:36 -0400

    Create default template for event registrations

--- single-tribe_events.php
+++ single-tribe_events.php
@@ -1,16 +1,13 @@
 <?php
-/**
- * Single Event Template - SPP Custom Override
- */
 get_header();
 ?>
-<div class="et_pb_row et_flex_row spp-two-col-row">
-    <div class="et_pb_column et_flex_column et_flex_column_18_24" id="content_column">
+<div style="display:flex; gap:20px;">
+    <div style="flex:3;">
         <?php while ( have_posts() ) : the_post(); ?>
             <?php tribe_get_template_part( 'single-event' ); ?>
         <?php endwhile; ?>
     </div>
-    <div class="et_pb_column et_flex_column et-last-child et_flex_column_6_24">
+    <div style="flex:1;">
         <?php echo do_shortcode('[spp_side_nav]'); ?>
     </div>
 </div>
