bab8c8cffe1150769e76ef5bd5311e091dd6b8a1
Gaetan Godin <gaetgodi@gmail.com>
2026-04-25 11:43:37 -0400

    Create default template for event registrations

---
+++ tribe/events/default-template.php
@@ -0,0 +1,14 @@
+<?php
+use Tribe\Events\Views\V2\Template_Bootstrap;
+get_header();
+?>
+<div class="et_pb_row et_flex_row spp-two-col-row">
+    <div class="et_pb_column et_flex_column et_flex_column_18_24" id="content_column">
+        <?php echo tribe( Template_Bootstrap::class )->get_view_html(); ?>
+    </div>
+    <div class="et_pb_column et_flex_column et-last-child et_flex_column_6_24">
+        <?php echo do_shortcode('[spp_side_nav]'); ?>
+    </div>
+</div>
+<?php
+get_footer();
