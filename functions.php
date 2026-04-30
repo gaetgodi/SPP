7c3b80787713f8054438466faf97920ba93a6186
Gaetan Godin <gaetgodi@gmail.com>
2026-04-29 20:32:47 -0400

    Trying to fix registration problems by restricting access to pages with code manager short codes

--- functions.php
+++ functions.php
@@ -84,3 +84,10 @@ add_action('wp_ajax_rtec_process_form_submission', function() {
     error_log('RTEC post status: ' . ($post ? $post->post_status : 'no post') . ' post_type: ' . ($post ? $post->post_type : 'n/a'));
     error_log('RTEC can_access: ' . (rtec_current_user_can_access_event($event_id) ? 'yes' : 'no'));
 }, 1);
+add_action('template_redirect', function() {
+    if (!empty($_POST['rtec_event_id'])) {
+        error_log('RTEC POST - user: ' . get_current_user_id() . 
+                  ' event: ' . $_POST['rtec_event_id'] . 
+                  ' post_keys: ' . implode(',', array_keys($_POST)));
+    }
+}, 1);
